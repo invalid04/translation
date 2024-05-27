d0c38dbec4fff4b109731e9940583370f86c799e
invalid04 <68178371+invalid04@users.noreply.github.com>
2024-05-27 01:13:02 -0500

    started web server

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from fastapi import FastAPI, BackgroundTasks
+from pydantic import BaseModel, validator 
+import tasks
+
+app = FastAPI()
+
 # Route 1 : /
 # Test if everything is working
+@app.get("/")
+def get_root():
+    return {"message": "Hello world"}
 
 # Route 2: /translate
 # take in a translation request, and store it to the db
